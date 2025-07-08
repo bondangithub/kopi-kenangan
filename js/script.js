520f974e9d7dff5da905c93fbd67c08593b19f2f
bondangithub <bondanputra0505@gmail.com>
2025-07-08 20:39:00 +0700

    website kopi kenangan

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika humberger menu di klik
+document.querySelector('#humberger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Klik diluar sidebar untuk menghilangkan nav
+const humberger = document.querySelector('#humberger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+})
